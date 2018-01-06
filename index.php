d363a7f81d316765fc8262c661419b00b1704d73
arubiewe <arubiewe@gmail.com>
2018-01-06 07:36:32 +0100

    My Tech Website Development Underconstruction

---
+++ index.php
@@ -0,0 +1,345 @@
+<!DOCTYPE html>
+<html>
+<head>
+<?php include_once("home.html"); ?>
+	<title>Developers, Lodge!</title>
+      <meta charset="utf-8" />
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.13.1/jquery.validate.min.js"></script>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
+        <link rel="stylesheet" href="css/Footer-with-social-icons.css">
+	</head>
+<body>
+	<br/>
+
+<nav class= "navbar navbar-inverse navbar-fixed-top ">
+  <div class="container-fluid">
+
+    <div class= "navbar-header">
+
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">CyberTech Solution</a>
+
+    </div>
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        
+    <ul class="nav navbar-nav">
+      <li> <a href="index">Home</a></li>
+      <li> <a href="#">About</a></li>
+      <li> <a href="#">Mission</a></li>
+      <li> <a href="#">Vision</a></li>
+      <li> <a href="#">Contact Us</a></li>
+
+      <li class="dropdown">
+      
+      <a class="dropdown-toggle" data-toggle="dropdown" href="#"> Other Links <span class="caret"></span></a>
+    
+
+      <ul class="dropdown-menu navbar-inverse">
+        <li> <a href="#"> News Update </a></li>
+        <li> <a href="#"> Admission </a></li>
+      </ul>
+
+      </li> 
+
+    </ul>
+
+
+    
+<ul class="nav navbar-nav navbar-right">
+
+    
+<!-- <li><p class="navbar-text">Already have an account?</p></li> -->
+        <li class="dropdown">
+          <!-- <a href="#" class="dropdown-toggle" data-toggle="dropdown"><b>Login</b> <span class="caret"></span></a> -->
+       <a href="#" span class="glyphicon glyphicon-log-in" class="dropdown-toggle" data-toggle="dropdown"><b> Login</b></span></a>
+      <ul id="login-dp" class="dropdown-menu">
+        <li>
+           <div class="row">
+              <div class="col-md-12">
+                <h5><strong>CYBER-TECH LOGIN</strong></h5>
+                Login via
+                <div class="social-buttons">
+                  <a href="#" class="btn btn-fb"><i class="fa fa-facebook"></i> Facebook</a>
+                  <a href="#" class="btn btn-tw"><i class="fa fa-twitter"></i> Twitter</a>
+                </div>
+                                or
+                 <form class="form" role="form" method="post" action="login" accept-charset="UTF-8" id="login-nav">
+                    <div class="form-group">
+                       <label class="sr-only" for="exampleInputEmail2">Email address</label>
+                       <input type="email" class="form-control" id="exampleInputEmail2" placeholder="Email address" required>
+                    </div>
+                    <div class="form-group">
+                       <label class="sr-only" for="exampleInputPassword2">Password</label>
+                       <input type="password" class="form-control" id="exampleInputPassword2" placeholder="Password" required>
+                                             <div class="help-block text-right"><a href="">Forget the password ?</a></div>
+                    </div>
+                    <div class="form-group">
+                       <button type="submit" class="btn btn-primary btn-block">Sign in</button>
+                    </div>
+                    <div class="checkbox">
+                       <label>
+                       <input type="checkbox"> keep me logged-in
+                       </label>
+                    </div>
+                 </form>
+              </div>
+              <div class="bottom text-center">
+                New here ? <a href="#"><b>Join Us</b></a>
+              </div>
+           </div>
+        </li>
+      </ul>
+        </li>
+     
+
+
+    <li><a href="#"><span class="glyphicon glyphicon-user"></span> Signup </a></li>
+  </ul>
+  
+
+</div>
+
+
+</nav>
+
+
+<div class="page-header">
+<br/>
+	<div class="alert alert-info">
+	<a href="#" class="close pull-left" data-dismiss="alert" aria-label="close"> &times;</a>
+		
+		<p> &nbsp; <b>Login uncessful,</b> please contact the site admin!</p>
+		
+	</div>
+
+<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+  </ol>
+
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner">
+    <div class="item active">
+      <img src="images/images.jpeg"   alt="...">
+      <div class="carousel-caption">
+        ...
+      </div>
+    </div>
+    <div class="item">
+      <img src="images/startup-photos.jpg"   alt="...">
+      <div class="carousel-caption">
+        ...
+      </div>
+    </div>
+    ...
+  </div>
+
+  <!-- Controls -->
+  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left"></span>
+  </a>
+  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right"></span>
+  </a>
+</div>
+
+<div class="jumbotron">
+	<h2 class="bannertext" align="left"> Welcome to official Cyber tech....</h2>
+		<p> We officially welcome you to our great page, here you meet different types of developers..... words <em> from Ab</em> </p>
+	</div>
+</div>
+
+<h4 class="introworld heading" align="center"> INTRODUCTION </h4>
+<p  class="introword" align="center"> CyberWorld is a website where different learning resources takes place. You are free to visit our official website anytime anyday for more learning resources. Our staff are walking to improve patnership with our client in order to foster relationship and promote their business to the next level.</p> 
+
+	
+	
+<!-- <nav class= "navbar navbar-inverse navbar-fixed-top "> -->
+	
+<div class="row" align="center">
+	
+<h3><strong>MEET THE DEVELOPERS!</strong></h3>	
+	
+</div>
+
+<div class="row">
+  <div class="col-sm-6 col-md-4">
+    <a href="#" class="thumbnail">
+      <img src="images/aru.jpg" height="300" width="300" alt="..." >
+      <h4 align="center"> ARUBIEWE WASIU <br/> PROGRAMMER <h4/>
+    </a>
+
+  </div>
+  <div class="col-sm-6 col-md-4">
+
+    <a href="#" class="thumbnail">
+      <img src="images/eskay.jpg" height="150" width="300" alt="...">
+      <center> 
+        <h4>AJAYI SIKIRU ADEKUNLE</h4>
+      </center>
+    </a>
+  </div>
+
+
+
+  <div class="col-sm-6 col-md-4">
+    <a href="#" class="thumbnail">
+      <img src="images/wale.jpg" height="150" width="300" alt="...">
+      <h4 align="center"> AJAYI NURUDEEN <br/>  SNR. WEB DEVELOPER/PROGRAMMER <h4/>
+    </a>
+  </div>
+</div>
+
+ <div class="row">
+
+      <div class="jumbotron" align="center" >
+      <p> Register With Us Now!</p>
+      <p><a class="btn btn-primary btn-lg" role="button" align-self-center>REGISTER NOW!</a></p>
+      </div>
+
+      <div class="jumbotron col-sm-6 " align="center">
+      <h4>OFFICE ADDRESS</h4>
+     <address>
+     <strong> Twitter, Inc.</strong><br>
+      12 Mercy Street, Iyesi Oko-Afo Badagry <br>
+      Lagos Nigeria, <br>
+  <abbr title="Phone">P:</abbr> +238137129870
+</address>
+
+<address>
+  <strong> ARUBIEWE WASIU ABIODUN S.</strong><br>
+  <a href="mailto:#">arubiewe@gmail.com</a>
+</address>
+</div>
+&nbsp;
+
+
+<div class="row col-md-6">
+<div id="carousel-example-generic1" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#carousel-example-generic1" data-slide-to="0" class="active"></li>
+    <li data-target="#carousel-example-generic1" data-slide-to="1"></li>
+    <li data-target="#carousel-example-generic1" data-slide-to="2"></li>
+  </ol>
+
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner">
+    <div class="item active">
+      <img src="images/asas.jpg"   alt="...">
+      <div class="carousel-caption">
+        
+      </div>
+    </div>
+    <div class="item">
+      <img src="images/web-portal.jpg"   alt="..." height="3500px">
+      <div class="carousel-caption">
+        <!-- <h3>Cross Section of Cyber Tech Team</h3> -->
+      </div>
+    </div>
+    
+  </div>
+
+  <!-- Controls -->
+  <a class="left carousel-control" href="#carousel-example-generic1" role="button" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left"></span>
+  </a>
+  <a class="right carousel-control" href="#carousel-example-generic1" role="button" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right"></span>
+  </a>
+</div>
+</div>
+
+
+
+
+
+</div>
+<div class="list-group col-sm-4 " >
+  <a href="#" class="list-group-item active">
+    <h4 class="list-group-item-heading">News heading</h4>
+    <p class="list-group-item-text">President Buhari has went to Libya to discus about the slave trade to come to an end</p>
+  </a>
+<a href="#" class="list-group-item active">
+    <h4 class="list-group-item-heading">News heading</h4>
+    <p class="list-group-item-text">President Buhari has went to Libya to discus about the slave trade to come to an end</p>
+  </a>
+
+</div>
+
+  
+<div class="content">
+	
+</div>
+
+    <footer id="myFooter">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-3 myCols">
+                    <h5>CyberTech</h5>
+                    <ul>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Sign up</a></li>
+                        <li><a href="#">Our staffs</a></li>
+                        <li><a href="#">News Flash</a></li>
+                        <li><a href="#">Meet up!</a></li>
+                    </ul>
+                </div>
+                <div class="col-sm-3 myCols">
+                    <h5>About us</h5>
+                    <ul>
+                        <li><a href="#">Company Information</a></li>
+                        <li><a href="#">Contact us</a></li>
+                        <li><a href="#">Reviews</a></li>
+                    </ul>
+                </div>
+                <div class="col-sm-3 myCols">
+                    <h5>Support</h5>
+                    <ul>
+                        <li><a href="#">FAQ</a></li>
+                        <li><a href="#">Help desk</a></li>
+                        <li><a href="#">Forums</a></li>
+                    </ul>
+                </div>
+                <div class="col-sm-3 myCols">
+                    <h5>Legal</h5>
+                    <ul>
+                        <li><a href="#">Terms of Service</a></li>
+                        <li><a href="#">Terms of Use</a></li>
+                        <li><a href="#">Privacy Policy</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="social-networks">
+            <a href="#" class="twitter"><i class="fa fa-twitter"></i></a>
+            <a href="#" class="facebook"><i class="fa fa-facebook-official"></i></a>
+            <a href="#" class="google"><i class="fa fa-google-plus"></i></a>
+        </div>
+        <div class="footer-copyright">
+            <p>© 2017 CyberTech Solution. All rights reserved! </p>
+        </div>
+    </footer>
+
+<script type="text/javascript" src="jquery.min.js"></script>
+
+<!-- <script type="text/javascript" src="js/bootstrap.min.js"></script>   --> 
+
+<script src="js/bootstrap.min.js"></script>
+
+
+
+  
+</body>
+</html>
